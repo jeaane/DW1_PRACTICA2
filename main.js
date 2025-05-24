4c43e6cb790595a6abd268d49ac0f03c3e732328
jean <dota666742@gmail.com>
2025-05-23 20:21:49 -0400

    practica2

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ function incrementar() {
   // Actualizamos el contenido del elemento con el nuevo valor
   document.getElementById('contador').textContent = valor;
 }
+function incrementar2() {
+  // Obtenemos el valor actual del contador
+  let valor = document.getElementById('contador-2').textContent;
+  // Convertimos el valor a un número entero
+  valor = parseInt(valor);
+  // Incrementamos el valor
+  valor++;
+  // Actualizamos el contenido del elemento con el nuevo valor
+  document.getElementById('contador-2').textContent = valor;
+}
+function decrementar() {
+  // Obtenemos el valor actual del contador
+  let valor = document.getElementById('contador').textContent;
+  // Convertimos el valor a un número entero
+  valor = parseInt(valor);
+  // Incrementamos el valor
+  valor++;
+  // Actualizamos el contenido del elemento con el nuevo valor
+  document.getElementById('contador').textContent = valor;
+}
+function decrementar2() {
+  // Obtenemos el valor actual del contador
+  let valor = document.getElementById('contador-2').textContent;
+  // Convertimos el valor a un número entero
+  valor = parseInt(valor);
+  // Incrementamos el valor
+  valor++;
+  // Actualizamos el contenido del elemento con el nuevo valor
+  document.getElementById('contador-2').textContent = valor;
+}
